7e42ec340f27e00011609a3796a22f8c89d5f224
anudeepti2004 <anudeepti2004@gmail.com>
2016-12-05 12:11:00 -0500

    Aligned grid to center of blackboard

--- js/example.js
+++ js/example.js
@@ -24,10 +24,16 @@ var modal3 = document.getElementById('myModal3');
 var blueName = "";
 var redName = "";
  
-
-var lastClicked;
 var grid = getNewGrid(); 
 
+var rulesOpen = false;
+var visited = new Array(table);
+for (var i = 0; i < table; i++) {
+   visited[i] = new Array(table);
+   for (var j = 0; j < table; j++){
+     visited[i][j] = 0;
+   }
+}
 
 function getNewGrid() {
 
@@ -69,7 +75,8 @@ function getNewGrid() {
 		setTimeout(function(){changeBlue()},1000);
 		hint.style.color = "#FF0000";
 		turnBlue=false;
-	}else{
+	}
+	else{
 		if(statusHint){
 		  el.className='redclicked';
 		  x[row-1][col-1]=1;
@@ -100,13 +107,6 @@ function getNewGrid() {
 		hint.style.color = "#0000FF";
 		turnBlue=true;
 	}
-	if(resultIsgameOver){
-		show();
-	}else{
-		lastClicked = el;
-	}
-    show();
-    
 });
   return newgrid; 
 }
@@ -170,9 +170,15 @@ function isGameOver(){
 // red 1 -> vertical, blue 0 -> horizontal, empty -1s
 
 function isThereAHorizontalPath(){
+    for (var i = 0; i < table; i++) {
+      for (var j = 0; j < table; j++){
+        visited[i][j] = 0;
+      }
+    }
     var ans = false;
     for(var i=0;i<table;i++) {
-       if(x[i][0] == 0) {
+       if(x[i][0] == 0 && visited[i][0] == 0) {
+         visited[i][0] = 1;
          ans = DFSHorizontal(i,0);
          if(ans){ 
             return ans;
@@ -186,28 +192,65 @@ function DFSHorizontal (i,j) {
    if(j == (table-1))
       return true;
    var ans = false;
-   if(x[i][j+1] == 0) {
+   // i, j+1 & i, j-1
+   if(x[i][j+1] == 0 && visited[i][j+1] == 0) {
+     visited[i][j+1] == 1; 
      ans = DFSHorizontal(i,j+1);
-     if(ans) { 
-       return ans;
-     }  
+     if(ans) return ans;
    }
-   if(i+1 < table && x[i+1][j+1] == 0) {
-     ans = DFSHorizontal(i+1,j+1);
-     if(ans) {
-       return ans;
+   if(j-1 >= 0 && x[i][j-1] == 0 && visited[i][j-1] == 0) {
+     visited[i][j-1] == 1; 
+     ans = DFSHorizontal(i,j-1);
+     if(ans) return ans;
+   }
+   // i+1, j+1 & i+1 , j & i+1, j-1
+   if(i+1 < table) {
+     if(x[i+1][j+1] == 0 && visited[i+1][j+1] == 0) {
+       visited[i+1][j+1] == 1;
+       ans = DFSHorizontal(i+1,j+1);
+       if(ans) return ans;
+     }
+     if(x[i+1][j] == 0 && visited[i+1][j] == 0) {
+       visited[i+1][j] == 1;
+       ans = DFSHorizontal(i+1,j);
+       if(ans) return ans;
+     }
+     if(j-1 >= 0 && x[i+1][j-1] == 0 && visited[i+1][j-1] == 0) {
+       visited[i+1][j-1] == 1;
+       ans = DFSHorizontal(i+1,j-1);
+       if(ans) return ans;
+     }  
+   }  
+   // i-1, j+1 & i-1, j & i-1, j-1    
+   if(i-1 >= 0){
+     if(x[i-1][j+1] == 0 && visited[i-1][j+1] == 0) {
+       visited[i-1][j+1] == 1;
+       ans = DFSHorizontal(i-1,j+1);
+       if(ans) return ans; 
      } 
-   }      
-   if(i-1 >= 0 && x[i-1][j+1] == 0) {
-     ans = DFSHorizontal(i-1,j+1); 
+     if(x[i-1][j] == 0 && visited[i-1][j] == 0) {
+       visited[i-1][j] == 1;
+       ans = DFSHorizontal(i-1,j);
+       if(ans) return ans; 
+     }  
+     if(j-1 >= 0 && x[i-1][j-1] == 0 && visited[i-1][j-1] == 0) {
+       visited[i-1][j-1] == 1;
+       ans = DFSHorizontal(i-1,j-1); 
+     }  
    }
    return ans;
 }
 
 function isThereAVerticalPath(){
+    for (var i = 0; i < table; i++) {
+      for (var j = 0; j < table; j++){
+        visited[i][j] = 0;
+      }
+    }
     var ans = false;
     for(var i=0;i<table;i++) {
-       if(x[0][i] == 1) {
+       if(x[0][i] == 1 && visited[0][i] == 0) {
+         visited[0][i] = 1;
          ans = DFSVertical(0,i);
          if(ans){ 
             return ans;
@@ -221,24 +264,54 @@ function DFSVertical (i,j) {
    if(i == (table-1))
       return true;
    var ans = false;
-   if(x[i+1][j] == 1) {
+   // i+1, j & i-1, j
+   if(x[i+1][j] == 1 && visited[i+1][j] == 0) {
+     visited[i+1][j] = 1;
      ans = DFSVertical(i+1,j);
-     if(ans) { 
-       return ans;
-     }  
+     if(ans) return ans;  
    }
-   if(j+1 < table && x[i+1][j+1] == 1) {
-     ans = DFSVertical(i+1,j+1);
-     if(ans) {
-       return ans;
+   if(i-1 >=0 && x[i-1][j] == 1 && visited[i-1][j] == 0) {
+     visited[i-1][j] = 1;
+     ans = DFSVertical(i-1,j);
+     if(ans) return ans;
+   }
+   // i+1, j+1 & i, j+1 & i-1, j+1
+   if(j+1 < table){ 
+     if(x[i+1][j+1] == 1 && visited[i+1][j+1] == 0) {
+       visited[i+1][j+1] = 1;
+       ans = DFSVertical(i+1,j+1);
+       if(ans) return ans;
      }
-   }       
-   if(j-1 >= 0 && x[i+1][j-1] == 1) {
-     ans = DFSVertical(i+1,j-1); 
+     if(x[i][j+1] == 1 && visited[i][j+1] == 0) {
+       visited[i][j+1] = 1;
+       ans = DFSVertical(i,j+1);
+       if(ans) return ans;
+     }
+     if(i-1 >=0 && x[i-1][j+1] == 1 && visited[i-1][j+1] == 0) {
+       visited[i-1][j+1] = 1;
+       ans = DFSVertical(i-1,j+1);
+       if(ans) return ans;
+     }
+   } 
+   // i+1, j-1 & i, j-1 & i-1, j-1      
+   if(j-1 >= 0){ 
+     if(x[i+1][j-1] == 1 && visited[i+1][j-1] == 0) {
+       visited[i+1][j-1] = 1;
+       ans = DFSVertical(i+1,j-1);
+       if(ans) return ans;
+     }
+     if(x[i][j-1] == 1 && visited[i][j-1] == 0) {
+       visited[i][j-1] = 1;
+       ans = DFSVertical(i,j-1);
+       if(ans) return ans;
+     }
+     if(i-1 >=0 && x[i-1][j-1] == 1 && visited[i-1][j-1] == 0) {
+       visited[i-1][j-1] = 1;
+       ans = DFSVertical(i-1,j-1);
+     }
    }
    return ans;
 }
-
 
 function getHint(){
    var textBox = document.createElement('myText');
@@ -270,6 +343,10 @@ function generateHint() {
   // hint type 4 : greater than x
   
   var hintType = Math.floor(Math.random() * 5);
+  if((num <=75 && hintType == 4) || (num >=25 && hintType == 3)) {
+      hintType = 1;
+  }
+  
   var upper, lower, sNum, digitLoc;
   var h = "";
   switch(hintType)
@@ -284,17 +361,21 @@ function generateHint() {
               }
               value2 = 0;
               break;
-     case 1 : while(true){
-				upper = Math.floor(Math.random() * num) + num + 1;
-				lower = Math.floor(Math.random() * num);
-				h = "Product is between " + lower + " and " +upper +", inclusive";
-				value1 = lower; value2 = upper;
-				var count=getCountWhereHintIsSatisfied(hintType,value1,value2);
+     case 1 : upper = Math.floor(Math.random() * 13);
+			  lower = Math.floor(Math.random() * 13);
+			  upper = upper + num; lower = num - lower;
+			  if(lower < 0) lower = 0;
+			  if((upper-lower) <= 1) {
+				upper += 2;
+			  }
+			  h = "Product is between " + lower + " and " +upper +", inclusive";
+			  value1 = lower; value2 = upper;
+				/*var count=getCountWhereHintIsSatisfied(hintType,value1,value2);
 				console.log(hintType,count);
 				if(count > 0 && count <= 10){
 						break;
-					}
 				}
+				*/
               break;
      case 2 : sNum = num.toString();
               digitLoc = Math.floor(Math.random() * sNum.length); 
@@ -302,22 +383,27 @@ function generateHint() {
               value1 = contains; value2 = 0; 
               h = "Product contains the digit "+contains;
               break;
-     case 3 : upper = Math.floor(Math.random() * num) + num + 1;
+     case 3 : upper = Math.floor(Math.random() * 13);
+              upper = upper + num + 1;
               h = "Product is less than " + upper;
               value1 = upper; value2 = 0;
               break;
-     case 4 : lower = Math.floor(Math.random() * num);
+     case 4 : lower = Math.floor(Math.random() * 13);
+              lower = num - lower - 1;
+              if(lower < 0) lower = 0;
               h = "Product is greater than " + lower;
               value1 = lower; value2 = 0;
-              break; 
-     
+              break;    
   }
   
   currentHintType = hintType;
+  console.log("generated Hint has Type:",currentHintType);
   return h;    
 }
 
 function satisfiesHint(row, col) {
+   console.log("checking if",i,j); 
+   console.log(" satisfies Hint Type:",currentHintType);
    if(x[row][col] != -1) {
       return false;
    }   
@@ -355,7 +441,6 @@ function satisfiesHint(row, col) {
 function clickStartGame() {
     clickRestartGame();
     modal1.style.display = "block";
-    //modal2.style.display = "block";
 }
 
 function clickRestartGame() {
@@ -370,6 +455,8 @@ function clickRestartGame() {
     statusHint = false;
     value1=0; value2=0;
 
+    rulesOpen = false; 
+     
     while(grid.hasChildNodes() ){
       grid.removeChild(grid.lastChild);
     }
@@ -397,11 +484,11 @@ function confirmGameDetails() {
     redBox.innerHTML = redName;
     var BN = document.getElementById('blueName');
     var RN = document.getElementById('redName');
-    if(BN.firstChild)
-      BN.removeChild(BN.firstChild);
-    if(RN.firstChild)  
-      RN.removeChild(RN.firstChild);
-    //BN.appendChild(blueBox);
+    while(BN.hasChildNodes())
+      BN.removeChild(BN.lastChild);
+    while(RN.hasChildNodes())  
+      RN.removeChild(RN.lastChild);
+    BN.appendChild(blueBox);
     RN.appendChild(redBox);
 	BN.appendChild(horizontalPathImage);
     RN.appendChild(verticalPathImage);
@@ -422,6 +509,7 @@ function modalClose() {
 // Handle ESC key (key code 27)
 document.addEventListener('keyup', function(e) {
     if (e.keyCode == 27) {
+	confirmGameDetails();
         modalClose();
     }
 });
@@ -438,18 +526,27 @@ modal1.addEventListener('click', modalClose, false);
 
 // Prevent event bubbling if click occurred within modal content body
 modal1.children[0].addEventListener('click', function(e) {
+confirmGameDetails();
     e.stopPropagation();
+	
 }, false);
 
 function clickClose() {
+	/*if (location.hash == '#myModal2') {
 		clickRestartGame();
 		modal2.style.display = "none";
+	}
+	
+	modal3.style.display = "none";*/
+	if(!rulesOpen) {
+	   	clickRestartGame();
+		modal2.style.display = "none";
+	}
+	else {
+	    rulesOpen = false; 
+	    modal3.style.display = "none";
+	}
 }
-
-function clickModal3Close() {
-		modal3.style.display = "none";
-}
-
 
 // Handle click on the modal container
 modal3.addEventListener('click', clickRulesClose, false);
@@ -460,7 +557,7 @@ modal3.children[0].addEventListener('click', function(e) {
 }, false);
 
 
-function getCountWhereHintIsSatisfied(hintType,v1,v2){
+/*function getCountWhereHintIsSatisfied(hintType,v1,v2){
 var index=0;
 	for (var i = 0; i < table; i++) {
      for(var j = 0; j< table; j++) {
@@ -491,8 +588,9 @@ var index=0;
      }
   }
   return index;
-}
+}*/
 
 function displayRules(){
-modal3.style.display = "block";
+   modal3.style.display = "block";
+   rulesOpen = true;
 }
